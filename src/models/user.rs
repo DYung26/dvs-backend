405142e75d74e53dfaf387b5d8d798d109852e5d
DYung26 <theadstore1407@gmail.com>
2025-06-29 07:07:14 +0100

    feat(models): enhance `User` struct with `Diesel` traits, switch `id` to `Uuid`, add `NewUser` insertable struct for `DB` operations

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,6 +1,33 @@
-#[derive(Debug, Serialize, Deserialize)]
+use diesel::{Queryable, Insertable, Selectable};
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+use crate::schema::users;
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Selectable)]
+#[diesel(table_name = users)]
 pub struct User {
-    pub id: i32,
-    pub name: String,
+    pub id: Uuid,
+    pub username: String,
     pub email: String,
+
+    #[serde(skip_serializing)]
+    pub password: String,
+    // pub role: UserRole,
 }
+
+#[derive(Debug, Deserialize, Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
+
+/*
+ * #[derive(Debug, Serialize, Deserialize)]
+ * pub enum UserRole {
+ *    Voter,
+ *    Candidate,
+ *    Committee,
+ * }
+ */
