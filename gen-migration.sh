509ef3399ca3e4a5c64350b18e6e03ecf9a2beef
DYung26 <theadstore1407@gmail.com>
2025-06-30 12:09:47 +0000

    chore(scripts): modify `gen-migration.sh` to run migrations and update schema instead of generating new ones

--- gen-migration.sh
+++ gen-migration.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-timestamp=$(date +%Y%m%d%H%M%S)
-name=${1:-migration}
-diesel migration generate "${timestamp}_${name}"
-# diesel migration run
+# echo "$ diesel setup"
+# diesel setup
+# timestamp=$(date +%Y%m%d%H%M%S)
+# name=${1:-migration}
+# echo "diesel migration generate <name>"
+# diesel migration generate "${timestamp}_${name}"
+echo "diesel migration run"
+diesel migration run
+echo "diesel print-schema > src/schema.rs"
+diesel print-schema > src/schema.rs
