d7b8b2dfb38d1d1c4a1d4367a88ee3002fdcc51a
DYung26 <theadstore1407@gmail.com>
2025-06-30 12:08:38 +0000

    feat(migrations): add `otps` table with foreign key to `users` and update `down.sql` to drop it

--- migrations/2025-06-29-025022_20250629025022_migration/up.sql
+++ migrations/2025-06-29-025022_20250629025022_migration/up.sql
@@ -7,3 +7,11 @@ CREATE TABLE users (
 	email VARCHAR NOT NULL unique,
 	password VARCHAR NOT NULL
 );
+
+CREATE TABLE otps (
+	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	user_id UUID NOT NULL unique,
+	email VARCHAR NOT NULL unique,
+	otp VARCHAR(6) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
