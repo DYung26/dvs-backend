86906207fc877b60c2680d8f77425ccac75556a1
DYung26 <theadstore1407@gmail.com>
2025-06-30 12:11:02 +0000

    feat(schema): add `otps` table definition, `join` constraint with `users`, and allow both in same query

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,16 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    otps (id) {
+        id -> Uuid,
+        user_id -> Uuid,
+        email -> Varchar,
+        #[max_length = 6]
+        otp -> Varchar,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Uuid,
         username -> Varchar,
@@ -8,3 +18,10 @@ diesel::table! {
         password -> Varchar,
     }
 }
+
+diesel::joinable!(otps -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    otps,
+    users,
+);
